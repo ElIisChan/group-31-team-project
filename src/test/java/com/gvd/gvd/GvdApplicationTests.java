c96f1f1059553955a60132fdf6ee72081dda0fd0
Ellis Crowther <ellis.crowther@gambitnash.co.uk>
2023-02-07 15:22:48 +0000

    feature(core): Initial push of Spring setup to repo

---
+++ src/test/java/com/gvd/gvd/GvdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvd.gvd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
